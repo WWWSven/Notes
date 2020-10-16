681993da321cdd1c2cde70e1a2670e7ce0f24e34
Cga <1981907427@qq.com>
2020-10-16 18:41:58 +0800

    autoCommit 2020-10-16 18:41:57

--- autoCommit.bat
+++ autoCommit.bat
@@ -6,16 +6,11 @@ set hour=%tt:~0,2%
 echo =======================================================
 echo          Starting autoCommit.bat
 echo =======================================================
-REM change file directory
 cd C:\Users\yh\Desktop\Programming\mdFile
-REM start git script 
 echo %~dp0
 git status
-if %hour% EQU 17 (
-    git commit -m "autoCommit %dd:/=-% %tt%"
-    git push origin master
-) else (
-    echo "=====> It's not time yet"
-)
+git add .
+git commit -m "autoCommit %dd:/=-% %tt%"
+git push origin master
 
 pause
